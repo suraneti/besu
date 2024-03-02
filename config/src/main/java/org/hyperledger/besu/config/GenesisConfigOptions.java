e229fe5af0fcd6bd25f0d4a396d5f7724dd2069f
Suraneti Rodsuwan <suraneti.rod@gmail.com>
2024-03-02 10:29:37 +0700

    chore: update isFixedBaseFee description
    Signed-off-by: Suraneti Rodsuwan <suraneti.rod@gmail.com>

--- config/src/main/java/org/hyperledger/besu/config/GenesisConfigOptions.java
+++ config/src/main/java/org/hyperledger/besu/config/GenesisConfigOptions.java
@@ -507,7 +507,7 @@ public interface GenesisConfigOptions {
   /**
    * Force a Base Fee as Gas Price network to used with London/EIP-1559.
    *
-   * @return true, if you want the next block to use the base fee as gas price.
+   * @return true, if you want the next block to use the gas price as the base fee.
    */
   boolean isFixedBaseFee();
 
